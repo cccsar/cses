fff3cac368f3db47d3a6f3f97cc31ebd46e678cd
cesar <15-11295@usbve>
2023-05-22 19:20:24 -0400

    Finished  but rustc  yet screams about it

--- src/graph/counting_rooms.rs
+++ src/graph/counting_rooms.rs
@@ -1,35 +1,73 @@
 type Graph<T> = Vec<Vec<T>>;
 
-pub fn solve(input : impl(Iterator<Item = String>), n : usize) {
+pub fn solve(input : impl(Iterator<Item = String>), n : usize, m : usize) -> isize {
     let mut graph : Graph<char> = vec![];
     let mut vtd : Vec< Vec<bool> > = vec![];
 
-    // Fill with values
-    (0..n).for_each( |_| { vtd.push( vec![false; n] ); });
 
     // Make every char acessible by indexing `graph`
-    input .for_each(|row| { 
-            graph.push( row.chars().collect::<Vec<char>>() ); 
-          });
+    for row in input 
+    {
+        graph.push( row.chars().collect::<Vec<char>>() ); 
+    }
 
+    // Fill with values
+    for i in 0..n 
+    {
+        vtd.push( vec![false;m] );
+    }
 
-    // TODO
+    let mut count = 0;
+    for (i, row)  in graph.iter().enumerate() 
+    {
+        for (j, _) in row.iter().enumerate() 
+        {
+            if graph[i][j] != '#' && !vtd[i][j] 
+            {
+                dfs(&graph, &mut vtd, (i as isize, j as isize) );
+                count += 1;
+            }
+        }
+    }
+
+    count
 
 }
 
-pub fn dfs(G : &Graph<char>, vtd : &mut Vec<Vec<bool>>, root : (usize, usize) ) {
-    vtd[root.0][root.1] = true;
+pub fn dfs(G : &Graph<char>, vtd : &mut Vec<Vec<bool>>, root : (isize, isize) ) {
 
+    vtd[root.0 as usize][root.1 as usize] = true;
 
-    // TODO
-
+    for ady in adyacents(&G, root)
+    {
+        if !vtd[ady.0 as usize][ady.1 as usize] 
+        {
+            dfs(G, vtd, ady);
+        }
+    }
 }
 
-// TODO
-/*
-pub fn adyacents(root : (usize, usize)) -> Vec<(usize, usize)> {
+
+pub fn adyacents(G: &Graph<char>, root : (isize, isize)) -> Box< dyn Iterator<Item = (isize, isize)> + '_> {
+    let mut candidates = Box::new( vec![ 
+        (root.0 + 1, root.1), 
+        (root.0 - 1, root.1), 
+        (root.0, root.1 + 1), 
+        (root.0, root.1 - 1), 
+        ]) ;
+
+
+
+    Box::new( 
+        candidates
+            .into_iter()
+            .filter( 
+                |(x,y)| 
+                    *x >= 0 && *y >= 0 && *x < (G.len() as isize) && *y < (G[0].len() as isize) 
+                    && G[*x as usize ][*y as usize] != '#'
+            ) 
+    )
 }
-*/
 
 #[cfg(test)]
 mod test {
@@ -38,14 +76,19 @@ mod test {
     #[test]
     fn base() {
         let n = 5;
+        let m = 8;
         let inp : String = 
-            "########
-            #..#...#
-            ####.#.#
-            #..#...#
-            ########".to_string() ;
+            "\
+########
+#..#...#
+####.#.#
+#..#...#
+########".to_string() ;
 
-        solve(inp.lines().map(|el| {el.to_string() }), n);
+        let input = inp.lines().map(|el| {el.to_string() });
+
+
+        assert_eq!( solve(input, n, m), 3) ;
 
     }
 
